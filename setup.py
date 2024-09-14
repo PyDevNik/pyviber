9824bb15782624e39275fe352e2fd683c76ae0e4
PyDevNik <107363471+PyDevNik@users.noreply.github.com>
2024-09-14 21:21:17 +0300

    add long desc

--- setup.py
+++ setup.py
@@ -3,10 +3,15 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
+with open('README.md') as md:
+    long_desc = md.read()
+
 setup(
         name="pyviber",
-        version="0.1",
+        version="0.2",
         description="Python package for developing Viber Bots",
         packages=find_packages(),
+        long_description=long_desc,
+        long_description_content_type='text/markdown',
         requires=required
 )
